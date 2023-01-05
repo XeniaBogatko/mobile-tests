d386f440f30a5811f3376e3caa5f25ee781207ce
Xenia Bogatko <bogatkok@gmail.com>
2023-01-05 23:50:01 +0100

    add language test added

--- src/test/java/tests/android/browserstack_sample/SearchTests.java
+++ src/test/java/tests/android/browserstack_sample/SearchTests.java
@@ -1,6 +1,5 @@
 package tests.android.browserstack_sample;
 
-import com.codeborne.selenide.Selenide;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
@@ -8,20 +7,15 @@ import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class SearchTests extends TestBase {
 
     @Test
     void successSearchTest() throws MalformedURLException, InterruptedException {
-        step("Skip onboarding pages", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_done_button")).click();
-        });
+        step("Skip onboarding pages", () -> back());
         step("Type search", () -> {
             $(AppiumBy.accessibilityId("Search Wikipedia")).click();
             $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Java");
@@ -32,26 +26,30 @@ public class SearchTests extends TestBase {
 
     @Test
     void openArticle() throws MalformedURLException, InterruptedException {
-        step("Skip onboarding pages", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_done_button")).click();
-        });
+        step("Skip onboarding pages", () -> back());
         step("Type search", () -> {
             $(AppiumBy.accessibilityId("Search Wikipedia")).click();
             $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Selenide");
         });
         step("Verify content found", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0)));
-        step("Choose page", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-            Selenide.back();
-            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-
-        });
+        step("Choose page", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click());
         step("Verify opened page", () ->
                 $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Selenide")));
     }
+
+    @Test
+    void addLanguage() throws MalformedURLException, InterruptedException {
+        step("Skip onboarding pages", () -> back());
+        step("Type search", () ->
+                $(AppiumBy.accessibilityId("Search Wikipedia")).click());
+        step("Add new language", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_lang_button")).click();
+            $$(AppiumBy.id("org.wikipedia.alpha:id/wiki_language_title")).findBy(text("ADD LANGUAGE")).click();
+            $$(AppiumBy.id("org.wikipedia.alpha:id/localized_language_name")).findBy(text("Deutsch")).click();
+        });
+        step("Verify added language", () ->
+                $$(AppiumBy.className("android.widget.TextView")).findBy(text("Deutsch")).shouldBe(visible));
+    }
 }
-//android.view.View android.widget.TextView
